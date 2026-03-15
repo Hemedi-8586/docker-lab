4dcfe0755a91390538ef969e76975d06bf0f4b32
hemedi mwarami hemedi <hemedi@localhost.localdomain>
2026-03-15 16:45:33 +0300

    Moved to workspace and organized structure

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'db-lab'; // Jina tuliloweka kwenye docker-compose
+$user = 'admin';
+$pass = 'hemedi_pass';
+$db   = 'mazoezi_db';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Duh! Database imekataa: " . $conn->connect_error);
+}
+
+echo "<h1>Hongera Hemedi! Website na Database zimeungana ndani ya Docker!</h1>";
+?>
